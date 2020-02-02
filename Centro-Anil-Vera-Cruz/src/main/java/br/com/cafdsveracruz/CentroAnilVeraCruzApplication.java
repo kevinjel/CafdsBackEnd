b1753a3a3fc691c2e4d1eb9cf9416f54270123e4
Matrix <Matrix@DESKTOP-G3E826M>
2020-02-02 20:29:13 -0300

    Projeto

---
+++ Centro-Anil-Vera-Cruz/src/main/java/br/com/cafdsveracruz/CentroAnilVeraCruzApplication.java
@@ -0,0 +1,13 @@
+package br.com.cafdsveracruz;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CentroAnilVeraCruzApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(CentroAnilVeraCruzApplication.class, args);
+	}
+
+}
---
+++ Centro-Anil-Vera-Cruz/src/test/java/br/com/cafdsveracruz/CentroAnilVeraCruzApplicationTests.java
@@ -0,0 +1,13 @@
+package br.com.cafdsveracruz;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class CentroAnilVeraCruzApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
